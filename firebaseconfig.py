ddd4910bd6d6df3093c58b0d7a917f230f82c348
EinarLop <einarlop0@gmail.com>
2023-06-09 16:06:49 -0600

    Envs

--- firebaseconfig.py
+++ firebaseconfig.py
@@ -2,20 +2,25 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import os
+import json
+
 
 # Use a service account.
-cred = credentials.Certificate({
+
+credents = {
   "type": "service_account",
   "project_id": "spomix-23189",
   "private_key_id": os.environ.get("private_key_id"),
-  "private_key": os.environ.get("private_key"),
+  "private_key": os.environ.get("private_key").replace(r'\n', '\n'),
   "client_email": os.environ.get("client_email"),
   "client_id": os.environ.get("client_id_fb"),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": os.environ.get("client_x509_cert_url"),
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-i19gj%40spomix-23189.iam.gserviceaccount.com"
-})
+}
+
+cred = credentials.Certificate(credents)
 
 app = firebase_admin.initialize_app(cred)
 
